25845ab4e57c7d8ff339dbf66596f3fa11205531
wilfreedi <nicolaa-designer@yandex.ru>
2026-04-22 12:14:52 +0500

    edit

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,6 +1,7 @@
 FROM php:8.3-cli-bookworm
 
-RUN apt-get update \
+RUN printf 'Acquire::Retries "5";\nAcquire::ForceIPv4 "true";\n' > /etc/apt/apt.conf.d/99codex-network \
+  && apt-get update \
   && apt-get install -y --no-install-recommends git unzip libzip-dev libsqlite3-dev \
   && docker-php-ext-install pdo_sqlite \
   && rm -rf /var/lib/apt/lists/*
